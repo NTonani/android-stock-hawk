2f91192bc4ad19e180ef7727e9e4321240c73776
Nathan Tonani <nathantonani@gmail.com>
2016-12-29 14:09:35 -0800

    Added boilerplate code / xml files / manifest additions for Widget support

---
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by ntonani on 12/29/16.
+ */
+
+public class StockWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
